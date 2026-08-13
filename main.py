01dc83db744c570d264a443ec039f4062afd2f50
Mag <166319982+magali-la@users.noreply.github.com>
2026-08-12 22:05:06 -0400

    test(main): add test for savings account methods

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # testing functionality of bankaccount - import the class
 from bank import BankAccount
+from savings import SavingsAccount
 
 # create instance of BankAccount and give it a value
 bob_account = BankAccount(2468, "Bob", 100)
@@ -27,3 +28,28 @@ try:
     bob_account.withdraw(126)
 except ValueError as error:
     print(error)
+
+
+# test for the savings account - interest rate is defaulted in the subclass definition so no need to declare it, but do override the amount instead of $0
+bob_savings = SavingsAccount(1357, "Bob", 100)
+print(f"Bob's starting balance in his savings account is: {bob_savings.balance}")
+
+# deposit - need to write the method separately since there is no return statement
+print("Bob is depositing $100 into savings")
+bob_savings.deposit(100)
+
+# this will print the interest rate as well
+print(bob_savings)
+
+# withdraw
+print("Bob wants to withdraw $50 from his savings")
+bob_savings.withdraw(50)
+
+print(bob_savings)
+
+# add interest
+print(f"Bob wants to see how much interest he has accumulated. The interest rate is {bob_savings.interest_rate * 100}%")
+
+bob_savings.apply_interest()
+
+print(bob_savings)
