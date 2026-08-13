027f35c3b3629df7c21c644c8f4d1dc463c15e71
Mag <166319982+magali-la@users.noreply.github.com>
2026-08-12 22:04:10 -0400

    feat(savings): add interest rate amethod and attribute, create override string with original string

--- savings.py
+++ savings.py
@@ -3,6 +3,21 @@ from bank import BankAccount
 
 # inherit the class as a parameter
 class SavingsAccount(BankAccount):
-    # initialize it - it's going to get all of the methods associated with BankAccount
-    def __init__(self, account_number, owner, balance=0):
+    # initialize it - add the interest rate parameter specific to this subclass at 5%
+    def __init__(self, account_number, owner, balance=0, interest_rate=.05):
+        # this gets the parent's methods
         super().__init__(account_number, owner, balance)
+
+        # initialize interest_rate attribute
+        self.interest_rate = interest_rate
+
+    # method to add interest - nothing else is used as an argument so you only need self
+    def apply_interest(self):
+        account_interest = self.balance * self.interest_rate
+
+        # add to the balance
+        self.balance += account_interest
+
+    # string override - make a new addition to the original string with info on the savings account
+    def __str__(self):
+        return f"{super().__str__()} \n Interest rate: {self.interest_rate * 100}%"
